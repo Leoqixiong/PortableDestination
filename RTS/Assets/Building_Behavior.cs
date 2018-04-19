aac10da23a97c24a3616268c126036b17cf23141
leosheung <xq8908@gmail.com>
2018-04-19 03:26:37 -0400

    First Test Project

---
+++ RTS/Assets/Building_Behavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building_Behavior : MonoBehaviour
+{
+    public Color original_color;
+    public Color target_color;
+    Renderer rend;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        original_color = rend.material.color;
+        target_color = rend.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rend.material.color != target_color)
+        {
+            rend.material.color = Color.Lerp(rend.material.color, target_color, 0.03f);
+        }
+	}
+
+    public void ChangeColor(Color col)
+    {
+        // rend.material.color = col;
+        target_color = col;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        // rend.material.color = original_color;
+        target_color = original_color;
+    }
+}
---
+++ RTS/Assets/Car_Behavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Behavior : MonoBehaviour {
+    [SerializeField]
+    Color field_function_color;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        // Debug.Log(other.gameObject.name);
+        if (other.gameObject.tag == "building")
+        {
+            other.GetComponent<Building_Behavior>().ChangeColor(field_function_color);
+        }
+    }
+}
